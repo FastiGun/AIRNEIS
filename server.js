a2ad2a012338a7c233b3a5249b3de93b473c7a67
Benjamin HEBERT <benjamin760080@gmail.com>
2023-03-15 09:21:17 +0100

    Fonctionnel

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(8080, function () {
+  console.log("Je tourne ici : http://localhost:8080");
 });
