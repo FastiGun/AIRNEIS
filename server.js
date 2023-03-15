cd447b391d4185695ab0312079348ab9f9c5af34
Benjamin HEBERT <benjamin760080@gmail.com>
2023-03-15 09:32:44 +0100

    Header et Footer ajoutés, juste une première ébauche

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
+app.use("/assets", express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.render("pages/index", { title: "Accueil" });
 });
 app.listen(8080, function () {
   console.log("Je tourne ici : http://localhost:8080");
