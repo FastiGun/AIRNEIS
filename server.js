50ba11cdb3f9ea32e103abc9ecab13c6c0b2766d
Benjamin HEBERT <benjamin760080@gmail.com>
2023-04-11 12:56:38 +0200

    Ajout des différentes pages blanches

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.use("/assets", express.static("public"));
 app.get("/", function (req, res) {
   res.render("pages/index", { title: "Accueil" });
 });
+
+app.get("/search", function (req, res) {
+  res.render("pages/search", { title: "Recherche" });
+});
+
+app.get("/cart", function (req, res) {
+  res.render("pages/cart", { title: "Panier" });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("pages/contact", { title: "Contact" });
+});
+
 app.listen(8080, function () {
   console.log("Je tourne ici : http://localhost:8080");
 });
