7107726c90349589c560c4a8368ed0101d9253d3
Benjamin HEBERT <83975236+FastiGun@users.noreply.github.com>
2023-05-18 17:57:21 +0200

    Delete produit directory

--- produit/routes.js
+++
@@ -1,9 +0,0 @@
-const { read, create, readOne, update, deleteOne } = require("./controller");
-
-module.exports = function (app, db) {
-  app.get("/api/produit", read(db));
-  app.post("/api/produit", create(db));
-  app.get("/api/produit/:id", readOne(db));
-  app.put("/api/produit/:id", update(db));
-  app.delete("/api/produit/:id", deleteOne(db));
-};
