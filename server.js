3b857df7213cc0f789d4a170a6241b891d8fdf66
Benjamin HEBERT <benjamin760080@gmail.com>
2023-05-18 16:38:41 +0200

    Passage sur mongoose pour la bdd

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const { connexion } = require("./db");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const PORT = 3001;
-const ASSETS_PATH = '/assets';
+const ASSETS_PATH = "/assets";
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const clientSchema = new mongoose.Schema({
+  id: Number,
+  nom: String,
+  prenom: String,
+  mail: String,
+  mdp: String,
+  telephone: String,
+  panier: Number,
+});
+
+const Client = mongoose.model("Client", clientSchema);
 
 connexion.then(async (db) => {
   const app = express();
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
   routes(app, db);
 
-  app.set('view engine', 'pug');
-  app.set('views', __dirname + '/views');
-  app.use(ASSETS_PATH, express.static('assets'));
+  app.set("view engine", "pug");
+  app.set("views", __dirname + "/views");
+  app.use(ASSETS_PATH, express.static("assets"));
 
   app.use((req, res, next) => {
     res.locals.assetsPath = ASSETS_PATH;
@@ -56,6 +78,26 @@ connexion.then(async (db) => {
     const collection = db.collection("client");
     const list = await collection.find({}).toArray();
     res.json(list);
+  });
+
+  app.post("/inscription", async (req, res) => {
+    const { id, nom, prenom, mail, mdp, telephone, panier } = req.body;
+    try {
+      const nouveauCompte = new Compte({
+        id,
+        nom,
+        prenom,
+        mail,
+        mdp,
+        telephone,
+        panier,
+      });
+      await nouveauCompte.save();
+      res.redirect("/");
+    } catch (erreur) {
+      console.error(erreur);
+      res.status(500).send("Erreur lors de la création du compte");
+    }
   });
 
   app.listen(PORT, () => {
