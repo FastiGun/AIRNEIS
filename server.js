fc4c6e75f893434b389efed1f0284e23779cccb8
Benjamin HEBERT <benjamin760080@gmail.com>
2023-05-21 16:34:41 +0200

    Ajout des récupérations de données sur différentes pages et ajout du type password sur les champs des forms

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
+const { string } = require("yup");
 require("dotenv").config();
 
 const PORT = 3001;
@@ -15,6 +16,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
+    //Client
     const clientSchema = new mongoose.Schema({
       id: { type: String, unique: true },
       nom: String,
@@ -22,10 +24,106 @@ mongoose
       mail: String,
       mdp: String,
       telephone: { type: String, default: null },
-      panier: { type: String, unique: true, default: uuidv4 },
     });
 
     const Client = mongoose.model("Client", clientSchema, "client");
+
+    //Panier
+    const panierSchema = new mongoose.Schema({
+      id: { type: String, unique: true },
+      client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+      article: { type: mongoose.Schema.Types.ObjectId, ref: "Produit" },
+      quantite: { type: Number, default: 0 },
+    });
+
+    const Panier = mongoose.model("Panier", panierSchema, "panier");
+
+    //Produit
+    const produitSchema = new mongoose.Schema({
+      id: { type: String, unique: true },
+      nom: String,
+      prix: Number,
+      stock: Number,
+      description: String,
+      categorie: { type: mongoose.Schema.Types.ObjectId, ref: "Categorie" },
+      photo: String,
+    });
+
+    const Produit = mongoose.model("Produit", produitSchema, "produit");
+
+    //Categorie
+    const categorieSchema = new mongoose.Schema({
+      id: { type: String, unique: true },
+      nom: String,
+    });
+
+    const Categorie = mongoose.model("Categorie", categorieSchema, "categorie");
+
+    //Adresse
+    const adresseSchema = new mongoose.Schema({
+      id: { type: String, unique: true },
+      client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+      nom: String,
+      rue: String,
+      ville: String,
+      cp: String,
+      num: String,
+      complement: String,
+    });
+
+    const Adresse = mongoose.model("Adresse", adresseSchema, "adresse");
+
+    //Paiement
+    const paiementSchema = new mongoose.Schema({
+      client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+      nom_carte: String,
+      num_carte: String,
+      date_expiration: String,
+      cvv: String,
+    });
+
+    const Paiement = mongoose.model("Paiement", paiementSchema, "paiement");
+
+    //Commande
+    const commandeSchema = new mongoose.Schema({
+      date: String,
+      prixHT: Number,
+      prixTTC: Number,
+      statut: String,
+      produits: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Produit",
+        },
+      ],
+      adresseFacturation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Adresse",
+      },
+      adresseLivraison: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Adresse",
+      },
+      client: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+    });
+
+    const Commande = mongoose.model("Commande", commandeSchema, "commande");
+
+    //Favoris
+    const favorisSchema = new mongoose.Schema({
+      categorie1: { type: mongoose.Schema.Types.ObjectId, ref: "Categorie" },
+      categorie2: { type: mongoose.Schema.Types.ObjectId, ref: "Categorie" },
+      categorie3: { type: mongoose.Schema.Types.ObjectId, ref: "Categorie" },
+      article1: { type: mongoose.Schema.Types.ObjectId, ref: "Produit" },
+      article2: { type: mongoose.Schema.Types.ObjectId, ref: "Produit" },
+      article3: { type: mongoose.Schema.Types.ObjectId, ref: "Produit" },
+      photo1: String,
+      photo2: String,
+      photo3: String,
+      photo4: String,
+    });
+
+    const Favoris = mongoose.model("Favoris", favorisSchema, "favoris");
 
     const app = express();
 
@@ -53,16 +151,58 @@ mongoose
       })
     );
 
-    app.get("/", function (req, res) {
-      res.render("pages/index", { title: "Accueil" });
+    app.get("/", async (req, res) => {
+      try {
+        const favoris = await Favoris.findOne({});
+        const categorie1 = await Categorie.findById(favoris.categorie1);
+        const categorie2 = await Categorie.findById(favoris.categorie2);
+        const categorie3 = await Categorie.findById(favoris.categorie3);
+        const article1 = await Produit.findById(favoris.article1);
+        const article2 = await Produit.findById(favoris.article2);
+        const article3 = await Produit.findById(favoris.article3);
+
+        res.render("pages/index", {
+          title: "Accueil",
+          favoris: {
+            categorie1,
+            categorie2,
+            categorie3,
+            article1,
+            article2,
+            article3,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .send("Une erreur est survenue lors de la récupération des favoris.");
+      }
     });
 
     app.get("/search", function (req, res) {
       res.render("pages/search", { title: "Recherche" });
     });
 
-    app.get("/cart", function (req, res) {
-      res.render("pages/cart", { title: "Panier" });
+    app.get("/cart", async (req, res) => {
+      if (!req.session.userId) {
+        // L'utilisateur n'est pas connecté, redirigez-le vers la page de connexion
+        return res.redirect("/connexion");
+      }
+      const clientId = req.session.userId;
+
+      try {
+        const paniers = await Panier.find({ id_client: clientId }).populate(
+          "produits"
+        );
+
+        res.render("pages/cart", { paniers, title: "Panier" });
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .send("Une erreur s'est produite lors de la récupération du panier.");
+      }
     });
 
     app.get("/contact", function (req, res) {
@@ -94,11 +234,18 @@ mongoose
       res.render("pages/inscription", { title: "Inscription" });
     });
 
-    /*app.get("/api/produit", async (req, res) => {
-      const collection = db.collection("produit");
-      const list = await collection.find({}).toArray();
-      res.json(list);
-    });*/
+    app.get("/api/produits", async (req, res) => {
+      try {
+        const produits = await Produit.find({});
+        res.json(produits);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          error:
+            "Une erreur s'est produite lors de la récupération des produits.",
+        });
+      }
+    });
 
     app.get("/api/client", async (req, res) => {
       const collection = Client.collection;
@@ -107,7 +254,7 @@ mongoose
     });
 
     app.post("/inscription", async (req, res) => {
-      const { id, nom, prenom, mail, mdp, telephone, panier } = req.body;
+      const { id, nom, prenom, mail, mdp, telephone } = req.body;
       const existingClient = await Client.findOne({ mail: mail });
       if (existingClient) {
         return res.status(409).send("Cet e-mail est déjà utilisé");
@@ -124,14 +271,11 @@ mongoose
           mail,
           mdp: empreinteMotDePasse,
           telephone,
-          panier,
         });
-        console.log(nouveauCompte);
         await nouveauCompte.save();
         req.session.userId = nouveauCompte.id;
         res.redirect("/");
       } catch (erreur) {
-        console.error(erreur);
         res.status(500).send("Erreur lors de la création du compte");
       }
     });
