48ad57a0207375f4f178f6d73f6f89ae964e0ac3
Benjamin HEBERT <benjamin760080@gmail.com>
2023-06-19 15:23:29 +0200

    email dans la req reset password

--- assets/scripts/reinitialisePassword.js
+++ assets/scripts/reinitialisePassword.js
@@ -1,5 +1,5 @@
 function forgotPassword() {
-    var email = document.getElementById('email').value;
+    var email = document.getElementById('mail').value;
 
     fetch('/api/reset-password', {
       method: 'POST',
@@ -10,7 +10,6 @@ function forgotPassword() {
     })
       .then(function(response) {
         if (response.ok) {
-            console.log(email)
             alert('Un email de réinitialisation a été envoyé à votre adresse email.');
         } else {
             throw new Error("Une erreur s'est produite lors de l'envoi de la demande de réinitialisation de mot de passe.");
