60a8152dcf4a8801c884d7c9009fc6f32159a077
Benjamin HEBERT <benjamin760080@gmail.com>
2023-06-19 15:18:05 +0200

    fix password change

--- assets/scripts/reinitialisePassword.js
+++ assets/scripts/reinitialisePassword.js
@@ -1,5 +1,5 @@
 function forgotPassword() {
-    var email = document.getElementById('mail').value;
+    var email = document.getElementById('email').value;
 
     fetch('/api/reset-password', {
       method: 'POST',
@@ -10,9 +10,10 @@ function forgotPassword() {
     })
       .then(function(response) {
         if (response.ok) {
-          alert('Un email de réinitialisation a été envoyé à votre adresse email.');
+            console.log(email)
+            alert('Un email de réinitialisation a été envoyé à votre adresse email.');
         } else {
-          throw new Error("Une erreur s'est produite lors de l'envoi de la demande de réinitialisation de mot de passe.");
+            throw new Error("Une erreur s'est produite lors de l'envoi de la demande de réinitialisation de mot de passe.");
         }
       })
       .catch(function(error) {
